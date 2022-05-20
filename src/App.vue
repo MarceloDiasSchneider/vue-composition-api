e11c4d265605a74ebc46a64485bfb8b321602a40
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-05-21 00:13:30 +0200

    save changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script setup>
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+import TheSection from "./components/TheSection.vue";
+import TheContent from "./components/TheContent.vue";
+import { reactive } from "@vue/reactivity";
 </script>
 
 <template>
   <header>
-    
+    <TheHeader header="header.text" message="message" />
   </header>
-
   <main>
+    <TheSection />
+    <TheContent />
   </main>
+
+  <footer class="text-muted py-5">
+    <TheFooter />
+  </footer>
 </template>
 
-<style>
-
-</style>
+<style></style>
